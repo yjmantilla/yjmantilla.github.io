5d0b47e7536691d69eac36fb849d45875de501b6
Yorguin Jose Mantilla <yjmantilla@gmail.com>
2020-01-02 02:07:49 -0500

    added interactivity to walkers

--- sims/random_walkers.js
+++ sims/random_walkers.js
@@ -1,9 +1,12 @@
 var numberOfWalkers = 1000;
 var walkers = new Array();
+var walkerSize = 1;
+var walkerSpeed = 1;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 for (var i=0;i < numberOfWalkers;i++){
-  walkers.push( new Walker(random(width),random(height),randomColor()));
+  //walkers.push( new Walker(random(width),random(height),randomColor(),random(1,20)));
+  walkers.push( new Walker(random(width),random(height),randomColor(),walkerSize));
 }
 background(color('black'));
 strokeWeight(5);
@@ -25,25 +28,47 @@ function windowResized() {
 }
 
 class Walker {
-  constructor(xPos,yPos,trailColor) {
+  constructor(xPos,yPos,trailColor,trailWeight) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.trailColor = trailColor;
+    this.trailWeight = trailWeight;
   }
   
   step(){
     var choices = [-1,0,1];
+    choices = choices.map(x => x * walkerSpeed);
     this.xPos += (random(choices));//upper limit is not included
     this.yPos += (random(choices));//playing with theses limits gives bias
   }
   
   display(){
+    push()
+    //strokeWeight(this.trailWeight);
     stroke(this.trailColor);
     point(this.xPos,this.yPos);
+    pop()
   }
 }
 
 function randomColor(){
   return color(random(255),random(255),random(255))
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    walkerSize -=1;
+  } else if (keyCode === RIGHT_ARROW) {
+    walkerSize+=1;
+  }
+  else if (keyCode === UP_ARROW){
+      walkerSpeed+=1;
+  }
+  else if (keyCode === DOWN_ARROW){
+      walkerSpeed-=1;
+  }
+  
+  strokeWeight(walkerSize);
+  console.log(walkerSpeed);
 }
 //mycar = new Car("Ford");
