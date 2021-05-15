69ee6dcce4ec58a09d84f420ea574cf60da2e122
Yorguin Jose Mantilla <yjmantilla@gmail.com>
2021-05-15 01:22:39 -0500

    web of knowledge initial commit

---
+++ chunks.py
@@ -0,0 +1,34 @@
+from os import listdir
+from os.path import isfile, join
+import re
+import json
+mypath = 'chunks'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+sources = []
+targets = []
+for i in range(len(onlyfiles)):
+    this_file = onlyfiles[i]
+    print(this_file)
+    title = ''
+    go_to = []
+    with open (join(mypath,this_file), "r") as myfile:
+        data=myfile.readlines()
+        #print(data)
+        title = data[0].replace('# ','').replace('\n','')
+        for line in data:
+            go_to = go_to + re.findall('\[\[(.*?)\]\]',line)
+    sources.append(title)
+    targets.append(go_to)
+    print('title',title)
+    print(go_to)
+
+# Get nodes:
+nodes = set(sources + [item for sublist in targets for item in sublist])
+nodes = [{'id':x} for x in nodes]
+links = [[{'source':source,'target':x} for x in target] for (source,target) in zip(sources,targets)]
+links = [item for sublist in links for item in sublist]
+graph = {'nodes':nodes,'links':links}
+
+with open("files\graph.json", "w") as out_file:
+    json.dump(graph, out_file)
---
+++ tutorials/foam.md
@@ -0,0 +1,5 @@
+# Foam
+
+The Foam for VSCode extension automatically generates Link Reference Definitions at the bottom of the file to make wiki-links compatible with Markdown tools and parsers.
+
+You can make a network of thoughts chunks or ideas with the help of <https://github.com/vasturiano/force-graph>
